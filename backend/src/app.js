c8fbdcde4a6f586e26477fedc97522ec730ce7c1
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-04-25 18:33:16 +0530

    backend fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://privguard-by-aniket.vercel.app",
 }));
 
 
